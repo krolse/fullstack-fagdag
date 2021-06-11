0c2a270e08c0624a166bb74c8533bddd38ddc697
hermannt <hermann.trampenau@bekk.no>
2021-06-11 12:59:14 +0200

    remove static header

--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -1,7 +1,6 @@
 function Restaurant(props) {
     return(
         <div className="wrapper">
-            <h1>Hallo</h1>
             <h3>{props.name}</h3>
             <p>{`Location: ${props.location}`}</p>
             <p>{`Score: ${props.score}`}</p>
